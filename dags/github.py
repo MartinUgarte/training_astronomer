f1ac24d9ed1bd18ccacb6c98fe79c8b93a22de39
Martin Ugarte <martinugarte2012@gmail.com>
2024-06-24 16:42:37 -0300

    Add a new DAG github

---
+++ dags/github.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    schedule=None,
+    catchup=False,
+    default_args={'retries': 2},
+    tags=['example']
+)
+def github():
+    @task()
+    
+    def my_task():
+        print("Hello Argentina!")
+    
+    my_task()
+
+github()
